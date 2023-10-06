66f7a1f9239c78efc5a411700a4f323627820635
Jan <jzawadzki200@gmail.com>
2023-10-06 21:57:54 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,21 @@
+const navToggle = document.querySelector('.navbar-toggler');
+const links = document.querySelectorAll('.link');
+
+const navToggler = () => {
+  links.forEach(link => {
+    link.classList.toggle('show');
+  });
+}
+
+
+navToggle.addEventListener('click', navToggler);
+
+
+links.forEach(link => {
+  link.addEventListener('click', () => {
+    links.forEach(link => {
+      link.classList.remove('show');
+    });
+  });
+});
+
---
+++ js/slide.js
@@ -0,0 +1,11 @@
+$('.fade').slick({
+	dots: false,
+	infinite: true,
+	speed: 500,
+	fade: true,
+	cssEase: 'linear',
+	prevArrow:
+		'<button type="button" class="slick-prev"><i class="fa-solid fa-arrow-left"></i></button>',
+	nextArrow:
+		'<button type="button" class="slick-next"><i class="fa-solid fa-arrow-right"></i></button>',
+});
