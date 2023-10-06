019ad34232dbb73e67e9371abce7919d1918a87a
Jan <jzawadzki200@gmail.com>
2023-10-06 22:01:38 +0200

    first commit

--- js/main.js
+++ js/main.js
@@ -2,20 +2,17 @@ const navToggle = document.querySelector('.navbar-toggler');
 const links = document.querySelectorAll('.link');
 
 const navToggler = () => {
-  links.forEach(link => {
-    link.classList.toggle('show');
-  });
-}
-
+	links.forEach((link) => {
+		link.classList.toggle('show');
+	});
+};
 
 navToggle.addEventListener('click', navToggler);
 
-
-links.forEach(link => {
-  link.addEventListener('click', () => {
-    links.forEach(link => {
-      link.classList.remove('show');
-    });
-  });
+links.forEach((link) => {
+	link.addEventListener('click', () => {
+		links.forEach((link) => {
+			link.classList.remove('show');
+		});
+	});
 });
-
